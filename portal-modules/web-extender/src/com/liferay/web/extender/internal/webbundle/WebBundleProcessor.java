865fbdac5506851bf4eb4e35849f4c8b9f72302d
Miguel Pastor <miguel.pastor@liferay.com>
2012-06-25 18:24:10 +0200

    Handle properly the deployment of bundles

--- portal-modules/web-extender/src/com/liferay/web/extender/internal/webbundle/WebBundleProcessor.java
+++ portal-modules/web-extender/src/com/liferay/web/extender/internal/webbundle/WebBundleProcessor.java
@@ -620,6 +620,11 @@ public class WebBundleProcessor implements ModuleFrameworkConstants {
 		List<String> requiredDeploymentContexts =
 			readPluginPackage.getRequiredDeploymentContexts();
 
+		if (requiredDeploymentContexts == null ||
+				requiredDeploymentContexts.isEmpty()) {
+			return;
+		}
+
 		int requiredDeploymentContextsSize = requiredDeploymentContexts.size();
 
 		StringBundler sb = new StringBundler(6*requiredDeploymentContextsSize);
--- portal-modules/web-extender/src/com/liferay/web/extender/internal/webbundle/WebBundleURLConnection.java
+++ portal-modules/web-extender/src/com/liferay/web/extender/internal/webbundle/WebBundleURLConnection.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.StreamUtil;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.module.framework.ModuleFrameworkConstants;
 import com.liferay.portal.util.FastDateFormatFactoryImpl;
 import com.liferay.portal.util.FileImpl;
@@ -89,10 +90,7 @@ public class WebBundleURLConnection extends URLConnection
 
 		URL innerURL = new URL(path);
 
-		File tempFile = FileUtil.createTempFile("war");
-
-		StreamUtil.transfer(
-			innerURL.openStream(), new FileOutputStream(tempFile));
+		File tempFile = transferToTempFolder(innerURL);
 
 		try {
 			WebBundleProcessor webBundleProcessor = new WebBundleProcessor(
@@ -105,6 +103,28 @@ public class WebBundleURLConnection extends URLConnection
 		finally {
 			tempFile.delete();
 		}
+	}
+
+	/**
+	 * This methods tries to keep the behaviour of the current deployment
+	 * mechanism. So the name of the file is being kept as it is.
+	 *
+	 * <br />
+	 *
+	 * TODO We need to improve the way the plugins are detected
+	 */
+	protected File transferToTempFolder(URL url) throws IOException {
+		File tempFolder = FileUtil.createTempFolder();
+
+		int start = url.getPath().lastIndexOf(StringPool.SLASH);
+
+		String fileName = url.getPath().substring(start + 1);
+
+		File tempFile = new File(tempFolder, fileName);
+
+		StreamUtil.transfer(url.openStream(), new FileOutputStream(tempFile));
+
+		return tempFile;
 	}
 
 	private ClassLoader _systemBundleClassLoader;
