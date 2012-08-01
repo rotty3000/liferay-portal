5719e46c824247f6842371fab5af7f5fc427aed2
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2012-08-01 16:52:34 +0200

    LPS-28990

--- portal-service/src/com/liferay/portal/util/Portal.java
+++ portal-service/src/com/liferay/portal/util/Portal.java
@@ -729,8 +729,6 @@ public interface Portal {
 	public HttpServletRequest getOriginalServletRequest(
 		HttpServletRequest request);
 
-	public String getOuterPortletId(HttpServletRequest request);
-
 	public long getParentGroupId(long scopeGroupId)
 		throws PortalException, SystemException;
 
