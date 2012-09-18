08546c9d723410f3a0550128b10068cfb79e5435
Raymond Auge <raymond.auge@liferay.com>
2012-09-18 14:36:37 -0400

    delegate registration of ServletContext to the PortalLifecycle

--- portal-impl/src/com/liferay/portal/spring/context/PortalContextLoaderListener.java
+++ portal-impl/src/com/liferay/portal/spring/context/PortalContextLoaderListener.java
@@ -36,6 +36,8 @@ import com.liferay.portal.kernel.util.ClearThreadLocalUtil;
 import com.liferay.portal.kernel.util.ClearTimerThreadUtil;
 import com.liferay.portal.kernel.util.InstancePool;
 import com.liferay.portal.kernel.util.MethodCache;
+import com.liferay.portal.kernel.util.PortalLifecycle;
+import com.liferay.portal.kernel.util.PortalLifecycleUtil;
 import com.liferay.portal.kernel.util.ReferenceRegistry;
 import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.kernel.webcache.WebCachePoolUtil;
@@ -149,7 +151,8 @@ public class PortalContextLoaderListener extends ContextLoaderListener {
 
 		InitUtil.init();
 
-		ServletContext servletContext = servletContextEvent.getServletContext();
+		final ServletContext servletContext =
+			servletContextEvent.getServletContext();
 
 		ClassPathUtil.initializeClassPaths(servletContext);
 
@@ -230,7 +233,20 @@ public class PortalContextLoaderListener extends ContextLoaderListener {
 		if (PropsValues.MODULE_FRAMEWORK_ENABLED) {
 			try {
 				ModuleFrameworkUtil.registerContext(applicationContext);
-				ModuleFrameworkUtil.registerContext(servletContext);
+
+				PortalLifecycleUtil.register(
+					new PortalLifecycle() {
+
+						public void portalInit() {
+							ModuleFrameworkUtil.registerContext(servletContext);
+						}
+
+						public void portalDestroy() {
+							// not needed
+						}
+
+					}
+				);
 
 				ModuleFrameworkUtil.startRuntime();
 			}
