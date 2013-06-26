080b98c7a968f51943d516bc8ffa4f5f36b67782
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2013-06-26 21:56:35 +0200

    LPS-37055 Centralize security check for p_p_auth token - portal-service - no change

--- portal-service/src/com/liferay/portal/security/auth/AuthTokenUtil.java
+++ portal-service/src/com/liferay/portal/security/auth/AuthTokenUtil.java
@@ -46,6 +46,14 @@ public class AuthTokenUtil {
 		return getAuthToken().getToken(request, plid, portletId);
 	}
 
+	public static boolean isTokenValid(
+		HttpServletRequest request, long plid, String portletId,
+		String strutsAction, String tokenValue) {
+
+		return getAuthToken().isTokenValid(
+			request, plid, portletId, strutsAction, tokenValue);
+	}
+
 	public void setAuthToken(AuthToken authToken) {
 		PortalRuntimePermission.checkSetBeanProperty(getClass());
 
