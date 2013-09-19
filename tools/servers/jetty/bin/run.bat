8353096c8ef915d898c3b69948bf842d32a61389
Raymond Auge <raymond.auge@liferay.com>
2013-09-19 15:29:16 -0400

    update jetty policy

--- tools/servers/jetty/bin/run.bat
+++ tools/servers/jetty/bin/run.bat
@@ -4,7 +4,7 @@ if "" == "%JAVA_HOME%" goto errorJavaHome
 
 pushd "%~p0" & pushd .. & set "JETTY_HOME=%cd%" & popd & popd
 
-set "JAVA_OPTS=-Dfile.encoding=UTF8 ${java.security.manager.option} -Djetty.home=%JETTY_HOME% -Djava.security.policy=%JETTY_HOME%\lib\policy\jetty.policy -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+set "JAVA_OPTS=-Dfile.encoding=UTF8 ${java.security.manager.option} -Djetty.home=%JETTY_HOME% -Djava.security.policy=%JETTY_HOME%\lib\policy\server.policy -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
 
 "%JAVA_HOME%/bin/java" %JAVA_OPTS% -jar %JETTY_HOME%/start.jar
 
--- tools/servers/jetty/bin/run.sh
+++ tools/servers/jetty/bin/run.sh
@@ -8,6 +8,6 @@ fi
 
 JETTY_HOME="$(cd "$(dirname "$0")/.." && pwd)"
 
-export JAVA_OPTS="-Dfile.encoding=UTF8 ${java.security.manager.option} -Djetty.home=$JETTY_HOME -Djava.security.policy=$JETTY_HOME/lib/policy/jetty.policy -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+export JAVA_OPTS="-Dfile.encoding=UTF8 ${java.security.manager.option} -Djetty.home=$JETTY_HOME -Djava.security.policy=$JETTY_HOME/lib/policy/server.policy -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
 
 $JAVA_HOME/bin/java $JAVA_OPTS -jar $JETTY_HOME/start.jar
