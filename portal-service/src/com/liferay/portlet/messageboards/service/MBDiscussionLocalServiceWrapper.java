edbc8eecaba595a4939ab0eb9ced02f3d8b72adc
Shinn Lok <shinn.lok@liferay.com>
2013-09-26 11:45:38 -0700

    LPS-38694 Regenerate

--- portal-service/src/com/liferay/portlet/messageboards/service/MBDiscussionLocalServiceWrapper.java
+++ portal-service/src/com/liferay/portlet/messageboards/service/MBDiscussionLocalServiceWrapper.java
@@ -358,6 +358,13 @@ public class MBDiscussionLocalServiceWrapper implements MBDiscussionLocalService
 	}
 
 	@Override
+	public com.liferay.portlet.messageboards.model.MBDiscussion fetchDiscussion(
+		java.lang.String className, long classPK)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _mbDiscussionLocalService.fetchDiscussion(className, classPK);
+	}
+
+	@Override
 	public com.liferay.portlet.messageboards.model.MBDiscussion getDiscussion(
 		long discussionId)
 		throws com.liferay.portal.kernel.exception.PortalException,
