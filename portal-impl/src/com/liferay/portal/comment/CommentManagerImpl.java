efe126880f502d3538e1a40a2b0abf554d399c51
Raymond Auge <raymond.auge@liferay.com>
2014-06-05 12:56:37 -0400

    LPS-46460 the generic comment manager impl is registry driven and null safe

---
+++ portal-impl/src/com/liferay/portal/comment/CommentManagerImpl.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.comment;
+
+import com.liferay.portal.kernel.comment.CommentManager;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.Function;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.registry.Filter;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceTracker;
+
+/**
+ * @author André de Oliveira
+ * @author Alexander Chow
+ * @author Raymond Augé
+ */
+public class CommentManagerImpl implements CommentManager {
+
+	public CommentManagerImpl() {
+		Registry registry = RegistryUtil.getRegistry();
+
+		Filter filter = registry.getFilter(
+			"(&(objectClass=" + CommentManager.class.getName() +
+				")(!(objectClass=" + getClass().getName() + ")))");
+
+		_serviceTracker = registry.trackServices(filter);
+
+		_serviceTracker.open();
+	}
+
+	@Override
+	public long addComment(
+			long userId, long groupId, String className, long classPK,
+			String userName, String subject, String body,
+			Function<String, ServiceContext> serviceContextFunction)
+		throws PortalException, SystemException {
+
+		if (_serviceTracker.isEmpty()) {
+			return 0L;
+		}
+
+		CommentManager commentManager = _serviceTracker.getService();
+
+		return commentManager.addComment(
+			userId, groupId, className, classPK, userName, subject, body,
+			serviceContextFunction);
+	}
+
+	@Override
+	public void addInitialDiscussion(
+			long userId, long groupId, String className, long classPK,
+			String userName)
+		throws PortalException, SystemException {
+
+		if (_serviceTracker.isEmpty()) {
+			return;
+		}
+
+		CommentManager commentManager = _serviceTracker.getService();
+
+		commentManager.addInitialDiscussion(
+			userId, groupId, className, classPK, userName);
+	}
+
+	@Override
+	public void deleteComment(long commentId)
+		throws PortalException, SystemException {
+
+		if (_serviceTracker.isEmpty()) {
+			return;
+		}
+
+		CommentManager commentManager = _serviceTracker.getService();
+
+		commentManager.deleteComment(commentId);
+	}
+
+	@Override
+	public void deleteDiscussion(String className, long classPK)
+		throws PortalException, SystemException {
+
+		if (_serviceTracker.isEmpty()) {
+			return;
+		}
+
+		CommentManager commentManager = _serviceTracker.getService();
+
+		commentManager.deleteDiscussion(className, classPK);
+	}
+
+	private ServiceTracker<CommentManager, CommentManager> _serviceTracker;
+
+}
