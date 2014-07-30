757d5b1e91a6a7a158d5953304f2e1f887df03ac
Carlos Sierra <carlos.sierra@liferay.com>
2014-07-30 12:59:10 +0200

    LPS-46978 Helper functions to get PortletRequest and PortletResponse

--- util-taglib/src/com/liferay/taglib/util/ExtensionTag.java
+++ util-taglib/src/com/liferay/taglib/util/ExtensionTag.java
@@ -14,11 +14,17 @@
 
 package com.liferay.taglib.util;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.JavaConstants;
+
 import com.liferay.registry.collections.ServiceTrackerMap;
 import com.liferay.registry.collections.ServiceTrackerMapFactory;
-
 import com.liferay.taglib.TagSupport;
 
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.servlet.ServletRequest;
 import javax.servlet.jsp.JspException;
 import java.util.List;
 
@@ -30,6 +36,28 @@ public class ExtensionTag extends TagSupport {
 	@Override
 	public int doEndTag() throws JspException {
 		return super.doEndTag();
+	}
+
+	protected PortletResponse getPorletResponse() {
+		return getFromConstant(JavaConstants.JAVAX_PORTLET_RESPONSE);
+	}
+
+	protected PortletRequest getPortletRequest() {
+		return getFromConstant(JavaConstants.JAVAX_PORTLET_REQUEST);
+	}
+
+	private <T> T getFromConstant(String constant) {
+
+		ServletRequest request = pageContext.getRequest();
+
+		T result = (T)request.getAttribute(constant);
+
+		if (result == null) {
+			throw new IllegalStateException(
+				"This tag can only be used inside a portlet");
+		}
+
+		return result;
 	}
 
 	@Override
@@ -53,6 +81,8 @@ public class ExtensionTag extends TagSupport {
 	}
 
 	private String _extensionId;
+
+	private static Log _log = LogFactoryUtil.getLog(ExtensionTag.class);
 
 	private static ServiceTrackerMap<String, List<ViewExtension>>
 		_extensions = ServiceTrackerMapFactory.createListServiceTracker(
