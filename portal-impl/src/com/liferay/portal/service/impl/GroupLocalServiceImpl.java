32ec57dbc231b2349ed6cdda801d0a566330bf5c
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2014-10-23 14:43:40 +0200

    LPS-51093 We need to create SCOPE_INDIVIDUAL resource permission for each group. Even Guest and personal sites need the individual resource permission record

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -372,13 +372,13 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 		layoutSetLocalService.addLayoutSet(groupId, false);
 
+		// Resources
+
+		resourceLocalService.addResources(
+			group.getCompanyId(), 0, 0, Group.class.getName(),
+			group.getGroupId(), false, false, false);
+
 		if ((classNameId == groupClassNameId) && !user.isDefaultUser()) {
-
-			// Resources
-
-			resourceLocalService.addResources(
-				group.getCompanyId(), 0, 0, Group.class.getName(),
-				group.getGroupId(), false, false, false);
 
 			// Site roles
 
@@ -400,15 +400,6 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 					userId, group, serviceContext.getAssetCategoryIds(),
 					serviceContext.getAssetTagNames());
 			}
-		}
-		else if (className.equals(Organization.class.getName()) &&
-				 !user.isDefaultUser()) {
-
-			// Resources
-
-			resourceLocalService.addResources(
-				group.getCompanyId(), 0, 0, Group.class.getName(),
-				group.getGroupId(), false, false, false);
 		}
 
 		addPortletDefaultData(group);
