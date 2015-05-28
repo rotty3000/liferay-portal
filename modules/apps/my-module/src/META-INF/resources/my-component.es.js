66cedf55b6b4fa797a6a4340f8a9db47c4a020b3
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-05-28 09:21:15 +0200

    Add another example module, add bower dependencies

---
+++ modules/apps/my-module/src/META-INF/resources/my-component.es.js
@@ -0,0 +1,5 @@
+define('my-module/my-component.es', ['exports', 'liferay/html/js/liferay/iliyan.es'], function(exports) {
+    exports.log = function(text) {
+        console.log(text);
+    };
+});
