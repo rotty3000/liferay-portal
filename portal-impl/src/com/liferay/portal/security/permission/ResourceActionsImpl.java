50934dc00bdf440b921c39d59aa64327b898bc2d
Máté Thurzó <mate.thurzo@liferay.com>
2015-07-28 21:29:00 +0200

    LPS-57489 Update ResourceActionsImpl with the new attribute handling

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.security.pacl.DoPrivileged;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -33,17 +34,13 @@ import com.liferay.portal.kernel.xml.DocumentType;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.UnsecureSAXReaderUtil;
 import com.liferay.portal.model.Group;
-import com.liferay.portal.model.LayoutPrototype;
-import com.liferay.portal.model.LayoutSetPrototype;
 import com.liferay.portal.model.Organization;
-import com.liferay.portal.model.PasswordPolicy;
 import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.PortletConstants;
 import com.liferay.portal.model.ResourceAction;
 import com.liferay.portal.model.Role;
 import com.liferay.portal.model.RoleConstants;
 import com.liferay.portal.model.User;
-import com.liferay.portal.model.UserGroup;
 import com.liferay.portal.service.GroupServiceUtil;
 import com.liferay.portal.service.PortletLocalService;
 import com.liferay.portal.service.ResourceActionLocalService;
@@ -51,8 +48,6 @@ import com.liferay.portal.service.RoleLocalService;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.PortletResourceBundles;
-import com.liferay.portlet.expando.model.ExpandoColumn;
-import com.liferay.portlet.mobiledevicerules.model.MDRRuleGroup;
 import com.liferay.util.JS;
 
 import java.io.InputStream;
@@ -79,18 +74,6 @@ import javax.servlet.http.HttpServletRequest;
 public class ResourceActionsImpl implements ResourceActions {
 
 	public void afterPropertiesSet() {
-		_organizationModelResources = new HashSet<>();
-
-		for (String resource : getOrganizationModelResources()) {
-			_organizationModelResources.add(resource);
-		}
-
-		_portalModelResources = new HashSet<>();
-
-		for (String resource : getPortalModelResources()) {
-			_portalModelResources.add(resource);
-		}
-
 		_portletResourceActionsBags = new HashMap<>();
 		_modelResourceActionsBags = new HashMap<>();
 
@@ -314,12 +297,12 @@ public class ResourceActionsImpl implements ResourceActions {
 
 	@Override
 	public String[] getOrganizationModelResources() {
-		return _ORGANIZATION_MODEL_RESOURCES;
+		return ArrayUtil.toStringArray(_organizationModelResources);
 	}
 
 	@Override
 	public String[] getPortalModelResources() {
-		return _PORTAL_MODEL_RESOURCES;
+		return ArrayUtil.toStringArray(_portalModelResources);
 	}
 
 	@Override
@@ -990,6 +973,20 @@ public class ResourceActionsImpl implements ResourceActions {
 
 		String name = modelResourceElement.elementTextTrim("model-name");
 
+		boolean organizationModelResource = GetterUtil.getBoolean(
+			modelResourceElement.attributeValue("organization"));
+
+		if (organizationModelResource) {
+			_organizationModelResources.add(name);
+		}
+
+		boolean portalModelResource = GetterUtil.getBoolean(
+			modelResourceElement.attributeValue("portal"));
+
+		if (portalModelResource) {
+			_portalModelResources.add(name);
+		}
+
 		ModelResourceActionsBag modelResourceActionsBag =
 			getModelResourceActionsBag(name);
 
@@ -1179,24 +1176,12 @@ public class ResourceActionsImpl implements ResourceActions {
 
 	private static final String _MODEL_RESOURCE_NAME_PREFIX = "model.resource.";
 
-	private static final String[] _ORGANIZATION_MODEL_RESOURCES = {
-		Organization.class.getName(), PasswordPolicy.class.getName(),
-		User.class.getName()
-	};
-
-	private static final String[] _PORTAL_MODEL_RESOURCES = {
-		ExpandoColumn.class.getName(), LayoutPrototype.class.getName(),
-		LayoutSetPrototype.class.getName(), MDRRuleGroup.class.getName(),
-		Organization.class.getName(), PasswordPolicy.class.getName(),
-		Role.class.getName(), User.class.getName(), UserGroup.class.getName()
-	};
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		ResourceActionsImpl.class);
 
 	private Map<String, ModelResourceActionsBag> _modelResourceActionsBags;
-	private Set<String> _organizationModelResources;
-	private Set<String> _portalModelResources;
+	private final Set<String> _organizationModelResources = new HashSet<>();
+	private final Set<String> _portalModelResources = new HashSet<>();
 	private Map<String, PortletResourceActionsBag> _portletResourceActionsBags;
 
 }
