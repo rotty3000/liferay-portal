1901c132346a9724e8cedbb4f24781c388bd2e68
Preston-Crary <preston.crary@liferay.com>
2015-10-28 14:08:44 -0700

    LPS-59894 Only add the branch ID if it's not the master branch - same thing we are already doing for the revision. Fixes running out of memory.

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/staging/StagingImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/staging/StagingImpl.java
@@ -61,6 +61,7 @@ import com.liferay.portal.model.GroupConstants;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutBranch;
 import com.liferay.portal.model.LayoutRevision;
+import com.liferay.portal.model.LayoutSetBranch;
 import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.StagedModel;
 import com.liferay.portal.model.User;
@@ -78,6 +79,7 @@ import com.liferay.portal.service.LayoutBranchLocalService;
 import com.liferay.portal.service.LayoutLocalService;
 import com.liferay.portal.service.LayoutRevisionLocalService;
 import com.liferay.portal.service.LayoutService;
+import com.liferay.portal.service.LayoutSetBranchLocalService;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.ServiceContextThreadLocal;
 import com.liferay.portal.service.UserLocalService;
@@ -1641,11 +1643,8 @@ public class StagingImpl implements Staging {
 		PortalPreferences portalPreferences =
 			PortletPreferencesFactoryUtil.getPortalPreferences(request);
 
-		portalPreferences.setValue(
-			Staging.class.getName(), getRecentLayoutSetBranchIdKey(layoutSetId),
-			String.valueOf(layoutSetBranchId));
-
-		ProxiedLayoutsThreadLocal.clearProxiedLayouts();
+		setRecentLayoutSetBranchId(
+			portalPreferences, layoutSetId, layoutSetBranchId);
 	}
 
 	@Override
@@ -1654,11 +1653,8 @@ public class StagingImpl implements Staging {
 
 		PortalPreferences portalPreferences = getPortalPreferences(user);
 
-		portalPreferences.setValue(
-			Staging.class.getName(), getRecentLayoutSetBranchIdKey(layoutSetId),
-			String.valueOf(layoutSetBranchId));
-
-		ProxiedLayoutsThreadLocal.clearProxiedLayouts();
+		setRecentLayoutSetBranchId(
+			portalPreferences, layoutSetId, layoutSetBranchId);
 	}
 
 	@Override
@@ -2349,14 +2345,35 @@ public class StagingImpl implements Staging {
 		_layoutService = layoutService;
 	}
 
+	@Reference(unbind = "-")
+	protected void setLayoutSetBranchLocalService(
+		LayoutSetBranchLocalService layoutSetBranchLocalService) {
+
+		_layoutSetBranchLocalService = layoutSetBranchLocalService;
+	}
+
 	protected void setRecentLayoutBranchId(
 		PortalPreferences portalPreferences, long layoutSetBranchId, long plid,
 		long layoutBranchId) {
 
-		portalPreferences.setValue(
-			Staging.class.getName(),
-			getRecentLayoutBranchIdKey(layoutSetBranchId, plid),
-			String.valueOf(layoutBranchId));
+		LayoutBranch layoutBranch = _layoutBranchLocalService.fetchLayoutBranch(
+			layoutBranchId);
+
+		if (layoutBranch == null) {
+			return;
+		}
+
+		if (layoutBranch.isMaster()) {
+			portalPreferences.setValue(
+				Staging.class.getName(),
+				getRecentLayoutBranchIdKey(layoutSetBranchId, plid), null);
+		}
+		else {
+			portalPreferences.setValue(
+				Staging.class.getName(),
+				getRecentLayoutBranchIdKey(layoutSetBranchId, plid),
+				String.valueOf(layoutBranchId));
+		}
 
 		ProxiedLayoutsThreadLocal.clearProxiedLayouts();
 	}
@@ -2400,6 +2417,33 @@ public class StagingImpl implements Staging {
 
 		setRecentLayoutBranchId(
 			portalPreferences, layoutSetBranchId, plid, layoutBranchId);
+	}
+
+	protected void setRecentLayoutSetBranchId(
+		PortalPreferences portalPreferences, long layoutSetId,
+		long layoutSetBranchId) {
+
+		LayoutSetBranch layoutSetBranch =
+			_layoutSetBranchLocalService.fetchLayoutSetBranch(
+				layoutSetBranchId);
+
+		if (layoutSetBranch == null) {
+			return;
+		}
+
+		if (layoutSetBranch.isMaster()) {
+			portalPreferences.setValue(
+				Staging.class.getName(),
+				getRecentLayoutSetBranchIdKey(layoutSetId), null);
+		}
+		else {
+			portalPreferences.setValue(
+				Staging.class.getName(),
+				getRecentLayoutSetBranchIdKey(layoutSetId),
+				String.valueOf(layoutSetBranchId));
+		}
+
+		ProxiedLayoutsThreadLocal.clearProxiedLayouts();
 	}
 
 	@Reference(unbind = "-")
@@ -2516,6 +2560,7 @@ public class StagingImpl implements Staging {
 	private LayoutLocalService _layoutLocalService;
 	private LayoutRevisionLocalService _layoutRevisionLocalService;
 	private LayoutService _layoutService;
+	private LayoutSetBranchLocalService _layoutSetBranchLocalService;
 	private StagingLocalService _stagingLocalService;
 	private UserLocalService _userLocalService;
 	private WorkflowInstanceLinkLocalService _workflowInstanceLinkLocalService;
