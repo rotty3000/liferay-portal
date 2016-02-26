b3a355da2726d3d2d88771eb781190db0cfa8c21
Carlos Sierra <carlos.sierra@liferay.com>
2016-02-26 14:57:14 +0100

    LPS-63730 Use a queue to store jobs
    This also uses a different thread than the one of the OSGi framework

--- modules/apps/platform/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/internal/release/ReleaseManager.java
+++ modules/apps/platform/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/internal/release/ReleaseManager.java
@@ -42,6 +42,9 @@ import java.io.OutputStream;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.Callable;
+import java.util.concurrent.LinkedBlockingDeque;
 
 import org.apache.felix.utils.log.Logger;
 
@@ -143,11 +146,15 @@ public class ReleaseManager {
 				new PropertyServiceReferenceComparator<UpgradeStep>(
 					"upgrade.from.schema.version")),
 			serviceTrackerMapListener);
+
+		_upgradeThread = new UpgradeThread();
 	}
 
 	@Deactivate
 	protected void deactivate() {
 		_serviceTrackerMap.close();
+
+		_upgradeThread.cancel();
 	}
 
 	protected void doExecute(
@@ -190,23 +197,11 @@ public class ReleaseManager {
 
 		OutputStream outputStream = outputStreamContainer.getOutputStream();
 
-		_outputStreamContainerFactoryTracker.runWithSwappedLog(
-			new UpgradeInfosRunnable(
-				bundleSymbolicName, upgradeInfos, outputStream),
-			outputStreamContainer.getDescription(), outputStream);
+		Callable<Void> upgradeCallable = new UpgradeCallable(
+			bundleSymbolicName, upgradeInfos, outputStream,
+			outputStreamContainer);
 
-		try {
-			outputStream.close();
-		}
-		catch (IOException ioe) {
-			throw new RuntimeException(ioe);
-		}
-
-		Release release = _releaseLocalService.fetchRelease(bundleSymbolicName);
-
-		if (release != null) {
-			_releasePublisher.publish(release);
-		}
+		_queue.push(upgradeCallable);
 	}
 
 	protected String getSchemaVersionString(String bundleSymbolicName) {
@@ -235,10 +230,12 @@ public class ReleaseManager {
 
 	private OutputStreamContainerFactoryTracker
 		_outputStreamContainerFactoryTracker;
+	private BlockingDeque<Callable<Void>> _queue = new LinkedBlockingDeque<>();
 	private ReleaseLocalService _releaseLocalService;
 	private ReleaseManagerConfiguration _releaseManagerConfiguration;
 	private ReleasePublisher _releasePublisher;
 	private ServiceTrackerMap<String, List<UpgradeInfo>> _serviceTrackerMap;
+	private UpgradeThread _upgradeThread;
 
 	private static class UpgradeServiceTrackerCustomizer
 		implements ServiceTrackerCustomizer<UpgradeStep, UpgradeInfo> {
@@ -291,6 +288,63 @@ public class ReleaseManager {
 
 	}
 
+	private class UpgradeCallable implements Callable<Void> {
+
+		public UpgradeCallable(
+			String bundleSymbolicName, List<UpgradeInfo> upgradeInfos,
+			OutputStream outputStream,
+			OutputStreamContainer outputStreamContainer) {
+
+			_bundleSymbolicName = bundleSymbolicName;
+			_upgradeInfos = upgradeInfos;
+			_outputStream = outputStream;
+			_outputStreamContainer = outputStreamContainer;
+		}
+
+		@Override
+		public Void call() throws Exception {
+			_outputStreamContainerFactoryTracker.runWithSwappedLog(
+				new UpgradeInfosRunnable(
+					_bundleSymbolicName, _upgradeInfos, _outputStream),
+				_outputStreamContainer.getDescription(), _outputStream);
+
+			try {
+				_outputStream.close();
+			}
+			catch (IOException ioe) {
+				throw new RuntimeException(ioe);
+			}
+
+			Release release = _releaseLocalService.fetchRelease(
+				_bundleSymbolicName);
+
+			if (release != null) {
+				_releasePublisher.publish(release);
+			}
+
+			return null;
+		}
+
+		@Override
+		public String toString() {
+			StringBundler sb = new StringBundler(_upgradeInfos.size() + 1);
+
+			sb.append("Task: ");
+
+			for (UpgradeInfo upgradeInfo : _upgradeInfos) {
+				sb.append(upgradeInfo.toString());
+			}
+
+			return sb.toString();
+		}
+
+		private final String _bundleSymbolicName;
+		private final OutputStream _outputStream;
+		private final OutputStreamContainer _outputStreamContainer;
+		private final List<UpgradeInfo> _upgradeInfos;
+
+	}
+
 	private class UpgradeInfoServiceTrackerMapListener
 		implements ServiceTrackerMapListener
 			<String, UpgradeInfo, List<UpgradeInfo>> {
@@ -330,6 +384,22 @@ public class ReleaseManager {
 				UpgradeStep upgradeStep = upgradeInfo.getUpgradeStep();
 
 				try {
+					String fromSchemaVersionString =
+						upgradeInfo.getFromSchemaVersionString();
+
+					String schemaVersionString = getSchemaVersionString(
+						_bundleSymbolicName);
+
+					if (!fromSchemaVersionString.equals(schemaVersionString)) {
+						_logger.log(
+							Logger.LOG_INFO,
+							"Skipping " + upgradeInfo +
+								". It has been executed in a previous " +
+									"planification");
+
+						continue;
+					}
+
 					upgradeStep.upgrade(
 						new DBProcessContext() {
 
@@ -359,6 +429,45 @@ public class ReleaseManager {
 		private final String _bundleSymbolicName;
 		private final OutputStream _outputStream;
 		private final List<UpgradeInfo> _upgradeInfos;
+
+	}
+
+	private class UpgradeThread extends Thread {
+
+		public void cancel() {
+			_canceled = true;
+
+			interrupt();
+		}
+
+		@Override
+		public void run() {
+			while (!isInterrupted() || isCanceled()) {
+				try {
+					Callable<Void> callable = _queue.takeLast();
+
+					callable.call();
+				}
+				catch (InterruptedException ie) {
+					_canceled = true;
+				}
+				catch (Exception e) {
+					_logger.log(Logger.LOG_ERROR, e.getMessage(), e);
+				}
+			}
+		}
+
+		private UpgradeThread() {
+			super("Upgrade Thread");
+			
+			start();
+		}
+
+		private boolean isCanceled() {
+			return _canceled;
+		}
+
+		private volatile boolean _canceled = false;
 
 	}
 
