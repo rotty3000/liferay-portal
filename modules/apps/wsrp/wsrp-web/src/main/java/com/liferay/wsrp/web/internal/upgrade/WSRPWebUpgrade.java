d4916ac1e2f00194ab9c8a80e99773c0593fd35b
Raymond Auge <raymond.auge@liferay.com>
2016-08-31 17:06:41 -0400

    LPS-65786 use field injection just to clean this up a little

--- modules/apps/wsrp/wsrp-web/src/main/java/com/liferay/wsrp/web/internal/upgrade/WSRPWebUpgrade.java
+++ modules/apps/wsrp/wsrp-web/src/main/java/com/liferay/wsrp/web/internal/upgrade/WSRPWebUpgrade.java
@@ -61,9 +61,7 @@ public class WSRPWebUpgrade implements UpgradeStepRegistrator {
 			"com.liferay.wsrp.web", "0.0.1", "1.0.0", new UpgradePortletId());
 	}
 
-	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
-	protected void setModuleServiceLifecycle(
-		ModuleServiceLifecycle moduleServiceLifecycle) {
-	}
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED)
+	private ModuleServiceLifecycle _moduleServiceLifecycle;
 
 }
