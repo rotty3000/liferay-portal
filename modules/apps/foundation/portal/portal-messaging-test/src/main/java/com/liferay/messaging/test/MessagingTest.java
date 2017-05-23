55d475f74fb1bebbfccfba48854d1811849727f4
Raymond Auge <raymond.auge@liferay.com>
2017-05-23 10:59:22 -0400

    update test dependencies

--- modules/apps/foundation/portal/portal-messaging-test/src/main/java/com/liferay/messaging/test/MessagingTest.java
+++ modules/apps/foundation/portal/portal-messaging-test/src/main/java/com/liferay/messaging/test/MessagingTest.java
@@ -1,14 +1,17 @@
 package com.liferay.messaging.test;
 
+import com.liferay.messaging.Message;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MessagingTest {
 
 	@Test
 	public void testOne() {
-//		Message message = new Message();
-//
-//		Assert.assertNotNull(message);
+		Message message = new Message();
+
+		Assert.assertNotNull(message);
 	}
 
 }
