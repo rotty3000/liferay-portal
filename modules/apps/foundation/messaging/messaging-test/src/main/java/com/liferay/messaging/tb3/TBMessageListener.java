591859bc78154bd30b5c48588c20a1002f0ffb26
Raymond Auge <raymond.auge@liferay.com>
2017-06-02 14:03:28 -0400

    LPS-73701 messaging-test

---
+++ modules/apps/foundation/messaging/messaging-test/src/main/java/com/liferay/messaging/tb3/TBMessageListener.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.messaging.tb3;
+
+import com.liferay.messaging.Message;
+import com.liferay.messaging.MessageListener;
+import com.liferay.messaging.MessageListenerException;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(
+	property = {
+		"destination.name=serial/test"
+	},
+	scope = ServiceScope.SINGLETON,
+	service = {Callable.class, MessageListener.class}
+)
+public class TBMessageListener implements Callable<Message>, MessageListener{
+
+	@Override
+	public void receive(Message message) throws MessageListenerException {
+		_message = message;
+
+		_latch.countDown();
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Received message {} from {}", message, this);
+		}
+	}
+
+	@Override
+	public Message call() throws Exception {
+		_latch.await(10, TimeUnit.SECONDS);
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Checking message {} from {}", _message, this);
+		}
+
+		return _message;
+	}
+
+	private static final Logger _log = LoggerFactory.getLogger(
+		TBMessageListener.class);
+
+	private final CountDownLatch _latch = new CountDownLatch(1);
+
+	private volatile Message _message;
+
+}
