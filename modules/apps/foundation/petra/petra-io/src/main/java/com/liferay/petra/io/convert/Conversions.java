d7a03f4bfea3ded1a585db8022e99726b92b35fe
Raymond Auge <raymond.auge@liferay.com>
2017-06-02 14:00:10 -0400

    petra-io

---
+++ modules/apps/foundation/petra/petra-io/src/main/java/com/liferay/petra/io/convert/Conversions.java
@@ -0,0 +1,168 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.petra.io.convert;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.osgi.util.converter.Converter;
+import org.osgi.util.converter.ConverterBuilder;
+import org.osgi.util.converter.Converting;
+import org.osgi.util.converter.Rule;
+import org.osgi.util.converter.StandardConverter;
+
+/**
+ * @author Jesse Rao
+ * @author Raymond Augé
+ */
+public class Conversions {
+
+	public static final String[] BOOLEANS = {"true", "t", "y", "on", "1"};
+
+	public static final boolean DEFAULT_BOOLEAN = false;
+
+	public static final double DEFAULT_DOUBLE = 0.0;
+
+	public static final int DEFAULT_INTEGER = 0;
+
+	public static final long DEFAULT_LONG = 0;
+
+	public static final short DEFAULT_SHORT = 0;
+
+	public static final String DEFAULT_STRING = "";
+
+	public static final String PASSWORD_MASK = "********";
+
+	public static Converting convert(Object object) {
+		return _instance._converter.convert(object);
+	}
+
+	public static <T> T convert(Object object, T defaultValue, Class<T> clazz) {
+		Converting converting = _instance._converter.convert(object);
+
+		converting.defaultValue(defaultValue);
+
+		return converting.to(clazz);
+	}
+
+	public static boolean getBoolean(Object object) {
+		return getBoolean(object, DEFAULT_BOOLEAN);
+	}
+
+	public static boolean getBoolean(Object object, boolean defaultValue) {
+		return convert(object, defaultValue, boolean.class);
+	}
+
+	public static double getDouble(Object object) {
+		return getDouble(object, DEFAULT_DOUBLE);
+	}
+
+	public static double getDouble(Object object, double defaultValue) {
+		return convert(object, defaultValue, double.class);
+	}
+
+	public static int getInteger(Object object) {
+		return getInteger(object, DEFAULT_INTEGER);
+	}
+
+	public static int getInteger(Object object, int defaultValue) {
+		return convert(object, defaultValue, int.class);
+	}
+
+	public static long getLong(Object object) {
+		return getLong(object, DEFAULT_LONG);
+	}
+
+	public static long getLong(Object object, long defaultValue) {
+		return convert(object, defaultValue, long.class);
+	}
+
+	public static short getShort(Object object) {
+		return getShort(object, DEFAULT_SHORT);
+	}
+
+	public static short getShort(Object object, short defaultValue) {
+		return convert(object, defaultValue, short.class);
+	}
+
+	public static String getString(Object object) {
+		return getString(object, DEFAULT_STRING);
+	}
+
+	public static String getString(Object object, String defaultValue) {
+		return convert(object, defaultValue, String.class);
+	}
+
+	private Conversions() {
+		ConverterBuilder builder =
+			new StandardConverter().newConverterBuilder();
+
+		final Converter rootConverter = builder.rule(
+				new Rule<String, Boolean>(
+					o -> {
+						if (Arrays.asList(BOOLEANS).contains(o)) {
+							return true;
+						}
+
+						return false;
+					}) {
+				}
+			).rule(
+				new Rule<Object, String>(
+					o -> {
+						if (o.getClass().equals(Object.class)) {
+							throw new RuntimeException();
+						}
+
+						return null;
+					}) {
+				}
+			).build();
+
+		builder = rootConverter.newConverterBuilder();
+
+		_converter = builder.rule(
+				new Rule<Map<String, Object>, String>(
+					o -> {
+						Map<String, Object> copy = new LinkedHashMap<>(o);
+
+						for (Map.Entry<String, Object> entry :
+								copy.entrySet()) {
+
+							Matcher matcher = _passwordPattern.matcher(
+								String.valueOf(entry.getKey()));
+
+							if (matcher.matches()) {
+								entry.setValue(PASSWORD_MASK);
+							}
+						}
+
+						return rootConverter.convert(copy).to(String.class);
+					}) {
+				}
+			).build();
+	}
+
+	private static final Conversions _instance = new Conversions();
+
+	private static final Pattern _passwordPattern = Pattern.compile(
+		"(?i).*password.*");
+
+	private final Converter _converter;
+
+}
---
+++ modules/apps/foundation/petra/petra-io/src/main/java/com/liferay/petra/io/nio/intraband/welder/WelderFactoryUtil.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.petra.io.nio.intraband.welder;
+
+import com.liferay.petra.io.convert.Conversions;
+import com.liferay.petra.io.internal.util.OSDetector;
+import com.liferay.petra.io.validate.Validator;
+import com.liferay.petra.io.nio.intraband.welder.fifo.FIFOUtil;
+import com.liferay.petra.io.nio.intraband.welder.fifo.FIFOWelder;
+import com.liferay.petra.io.nio.intraband.welder.socket.SocketWelder;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class WelderFactoryUtil {
+
+	public static Welder createWelder() {
+		Class<? extends Welder> welderClass = getWelderClass();
+
+		try {
+			return welderClass.newInstance();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(
+				"Unable to create Welder instance for class " + welderClass, e);
+		}
+	}
+
+	public static Class<? extends Welder> getWelderClass() {
+		if (Validator.isNotNull(_INTRABAND_WELDER_IMPL)) {
+			try {
+				return Class.forName(
+					_INTRABAND_WELDER_IMPL).asSubclass(Welder.class);
+			}
+			catch (ClassNotFoundException cnfe) {
+				throw new RuntimeException(
+					"Unable to load class with name " + _INTRABAND_WELDER_IMPL,
+					cnfe);
+			}
+		}
+		else {
+			if (!OSDetector.isWindows() && FIFOUtil.isFIFOSupported()) {
+				return FIFOWelder.class;
+			}
+			else {
+				return SocketWelder.class;
+			}
+		}
+	}
+
+	private static final String _INTRABAND_WELDER_IMPL = Conversions.getString(
+		System.getProperty("intraband.welder.impl"));
+
+}
