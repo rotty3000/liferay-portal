0cd6f8b61f8f34e33fa8ce6a1e8e6841fcec50a7
Raymond Auge <raymond.auge@liferay.com>
2017-06-15 16:04:14 -0400

    petra-io

--- modules/apps/foundation/petra/petra-io/src/test/java/com/liferay/petra/io/DeserializerTest.java
+++ modules/apps/foundation/petra/petra-io/src/test/java/com/liferay/petra/io/DeserializerTest.java
@@ -14,7 +14,6 @@
 
 package com.liferay.petra.io;
 
-import com.liferay.petra.io.internal.loader.ClassLoaderPoolUtil;
 import com.liferay.petra.io.unsync.UnsyncByteArrayOutputStream;
 import com.liferay.portal.kernel.test.rule.CodeCoverageAssertor;
 import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
