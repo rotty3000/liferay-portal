7f13ca5e586c15d1b0cdfefbed3ed3091fd61be5
Jesse Rao <jesse.rao@liferay.com>
2017-06-16 17:04:33 -0400

    LPS-73701 Add tests

---
+++ modules/apps/foundation/messaging/messaging-test/src/main/java/com/liferay/messaging/tb16/ExecutorServiceRegistrarImpl.java
@@ -0,0 +1,22 @@
+package com.liferay.messaging.tb16;
+
+import com.liferay.messaging.ExecutorServiceRegistrar;
+
+import java.util.concurrent.ExecutorService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+@Component(
+	property = {"destination.name=parallel/test"},
+	scope = ServiceScope.SINGLETON,
+	service = ExecutorServiceRegistrar.class
+)
+public class ExecutorServiceRegistrarImpl implements ExecutorServiceRegistrar {
+
+	@Override
+	public <T extends ExecutorService> T registerExecutorService(String name, T executorService) {
+		return null;
+	}
+
+}
