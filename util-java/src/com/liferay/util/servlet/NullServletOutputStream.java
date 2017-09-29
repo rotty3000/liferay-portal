b046f39309dd8ff54cf0af0fc44dfe373ebc6038
Cristina Rodriguez <cristina.rodriguez@liferay.com>
2017-09-29 15:13:20 -0700

    LPS-73784 Implement ServletOutputStream methods from Servlet 3.1

--- util-java/src/com/liferay/util/servlet/NullServletOutputStream.java
+++ util-java/src/com/liferay/util/servlet/NullServletOutputStream.java
@@ -15,12 +15,28 @@
 package com.liferay.util.servlet;
 
 import javax.servlet.ServletOutputStream;
+import javax.servlet.WriteListener;
 
 /**
  * @author Brian Wing Shun Chan
  */
 public class NullServletOutputStream extends ServletOutputStream {
 
+	@Override
+	public boolean isReady() {
+		return true;
+	}
+
+	/**
+	* @since Servlet 3.1
+	*/
+	@Override
+	public void setWriteListener(WriteListener listener) {
+	}
+
+	/**
+	* @since Servlet 3.1
+	*/
 	@Override
 	public void write(int b) {
 	}
