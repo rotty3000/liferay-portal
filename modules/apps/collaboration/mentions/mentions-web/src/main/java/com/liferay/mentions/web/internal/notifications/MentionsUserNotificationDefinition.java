ee422d7fd0f89f38010636c464dbf3daeecdb174
Jesse Rao <jesse.rao@liferay.com>
2017-11-08 15:33:43 -0500

    LPS-75566 Mentions

--- modules/apps/collaboration/mentions/mentions-web/src/main/java/com/liferay/mentions/web/internal/notifications/MentionsUserNotificationDefinition.java
+++ modules/apps/collaboration/mentions/mentions-web/src/main/java/com/liferay/mentions/web/internal/notifications/MentionsUserNotificationDefinition.java
@@ -26,7 +26,6 @@ import org.osgi.service.component.annotations.Component;
  * @author Sergio González
  */
 @Component(
-	immediate = true,
 	property = {"javax.portlet.name=" + MentionsPortletKeys.MENTIONS},
 	service = UserNotificationDefinition.class
 )
