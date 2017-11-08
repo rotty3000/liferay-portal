cd5af421961d8be88ce94b1eac443003c112d1a6
Jesse Rao <jesse.rao@liferay.com>
2017-11-08 16:01:43 -0500

    LPS-75566 Notifications

--- modules/apps/collaboration/notifications/notifications-web/src/main/java/com/liferay/notifications/web/internal/poller/NotificationsPollerProcessor.java
+++ modules/apps/collaboration/notifications/notifications-web/src/main/java/com/liferay/notifications/web/internal/poller/NotificationsPollerProcessor.java
@@ -29,7 +29,6 @@ import org.osgi.service.component.annotations.Reference;
  * @author Sergio González
  */
 @Component(
-	immediate = true,
 	property = {"javax.portlet.name=" + NotificationsPortletKeys.NOTIFICATIONS},
 	service = PollerProcessor.class
 )
--- modules/apps/collaboration/notifications/notifications-web/src/main/java/com/liferay/notifications/web/internal/portlet/NotificationsPortlet.java
+++ modules/apps/collaboration/notifications/notifications-web/src/main/java/com/liferay/notifications/web/internal/portlet/NotificationsPortlet.java
@@ -46,7 +46,6 @@ import org.osgi.service.component.annotations.Reference;
  * @author Sergio González
  */
 @Component(
-	immediate = true,
 	property = {
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=notifications-portlet",
