3f35d4c642a4670aaecfccc60e1d2100d0334f36
Jesse Rao <jesse.rao@liferay.com>
2017-11-09 16:34:48 -0500

    LPS-75566 My Account

--- modules/apps/foundation/my-account/my-account-web/src/main/java/com/liferay/my/account/web/internal/portlet/action/EditUserMVCActionCommand.java
+++ modules/apps/foundation/my-account/my-account-web/src/main/java/com/liferay/my/account/web/internal/portlet/action/EditUserMVCActionCommand.java
@@ -35,7 +35,6 @@ import org.osgi.service.component.annotations.Component;
  * @author Brian Wing Shun Chan
  */
 @Component(
-	immediate = true,
 	property = {
 		"javax.portlet.name=" + MyAccountPortletKeys.MY_ACCOUNT,
 		"mvc.command.name=/users_admin/edit_user"
