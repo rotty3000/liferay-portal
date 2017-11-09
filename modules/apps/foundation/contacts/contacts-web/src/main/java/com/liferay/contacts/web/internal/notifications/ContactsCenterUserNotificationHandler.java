6fcc2c8b80b2ff2998e320b61058a11a25e521c5
Jesse Rao <jesse.rao@liferay.com>
2017-11-09 15:43:34 -0500

    LPS-75566 Contacts

--- modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/internal/notifications/ContactsCenterUserNotificationHandler.java
+++ modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/internal/notifications/ContactsCenterUserNotificationHandler.java
@@ -51,7 +51,6 @@ import org.osgi.service.component.annotations.Reference;
  * @author Jonathan Lee
  */
 @Component(
-	immediate = true,
 	property = {"javax.portlet.name=" + ContactsPortletKeys.CONTACTS_CENTER},
 	service = UserNotificationHandler.class
 )
