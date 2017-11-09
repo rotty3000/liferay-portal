bb4d30eb8ab1a0948334db4116325ca35bf4b4dc
Jesse Rao <jesse.rao@liferay.com>
2017-11-09 10:20:18 -0500

    LPS-75566 Currency Converter, Dictionary, Document Library, Flash

--- modules/apps/currency-converter/currency-converter-web/src/main/java/com/liferay/currency/converter/web/internal/portlet/validator/CurrencyConverterPreferencesValidator.java
+++ modules/apps/currency-converter/currency-converter-web/src/main/java/com/liferay/currency/converter/web/internal/portlet/validator/CurrencyConverterPreferencesValidator.java
@@ -31,7 +31,6 @@ import org.osgi.service.component.annotations.Component;
  * @author Brian Wing Shun Chan
  */
 @Component(
-	immediate = true,
 	property = {
 		"javax.portlet.name=" + CurrencyConverterPortletKeys.CURRENCY_CONVERTER
 	}
--- modules/apps/document-library-file-version-discussion/document-library-file-version-discussion-web/src/main/java/com/liferay/document/library/file/version/discussion/web/internal/util/DiscussionDLFileVersionPolicy.java
+++ modules/apps/document-library-file-version-discussion/document-library-file-version-discussion-web/src/main/java/com/liferay/document/library/file/version/discussion/web/internal/util/DiscussionDLFileVersionPolicy.java
@@ -26,7 +26,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Adolfo Pérez
  */
-@Component(immediate = true)
+@Component
 public class DiscussionDLFileVersionPolicy implements DLFileVersionPolicy {
 
 	@Override
