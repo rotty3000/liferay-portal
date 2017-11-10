65a41343ac8288bf85f75da6a8ae220c83cd8d82
Jesse Rao <jesse.rao@liferay.com>
2017-11-10 13:13:08 -0500

    LPS-75566 Web Form

--- modules/apps/web-form/web-form-web/src/main/java/com/liferay/web/form/web/internal/portlet/WebFormPortlet.java
+++ modules/apps/web-form/web-form-web/src/main/java/com/liferay/web/form/web/internal/portlet/WebFormPortlet.java
@@ -82,7 +82,6 @@ import org.osgi.service.component.annotations.Reference;
  * @author Peter Fellwock
  */
 @Component(
-	immediate = true,
 	property = {
 		"com.liferay.portlet.css-class-wrapper=web-form-portlet",
 		"com.liferay.portlet.display-category=category.tools",
