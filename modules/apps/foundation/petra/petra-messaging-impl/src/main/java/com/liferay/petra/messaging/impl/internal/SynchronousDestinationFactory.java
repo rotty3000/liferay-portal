d78a1e35c2c8aa47d3810a158d0f6d5759f9e2b5
Jesse Rao <jesse.rao@liferay.com>
2018-02-27 12:33:44 -0500

    LPS-73701 petra messaging impl

---
+++ modules/apps/foundation/petra/petra-messaging-impl/src/main/java/com/liferay/petra/messaging/impl/internal/SynchronousDestinationFactory.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.petra.messaging.impl.internal;
+
+import com.liferay.petra.messaging.api.DestinationSettings;
+import com.liferay.petra.messaging.spi.SynchronousDestination;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * <p>
+ * Destination that delivers a message to a list of message listeners in
+ * parallel.
+ * </p>
+ *
+ * @author Raymond Augé
+ */
+@Component(
+	factory = "synchronous.destination",
+	service = SynchronousDestination.class
+)
+public class SynchronousDestinationFactory extends SynchronousDestination {
+
+	@Activate
+	protected void activate(DestinationSettings destinationSettings) {
+		setName(destinationSettings.destination_name());
+		afterPropertiesSet();
+		open();
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		close();
+	}
+
+}
