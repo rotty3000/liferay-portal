ead8609c389c64f9f94389cf12080719d18f3961
Hugo Huijser <hugo.huijser@liferay.com>
2019-10-14 11:07:39 -0700

    LPS-102598 Update versions Export-Packages in modules/core/portal-bootstrap/system.packages.extra.bnd according to the versions defined in lib/*/dependencies.properties

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/util/BNDSourceUtil.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/util/BNDSourceUtil.java
@@ -14,17 +14,33 @@
 
 package com.liferay.source.formatter.checks.util;
 
+import aQute.bnd.header.Attrs;
+import aQute.bnd.header.Parameters;
 import aQute.bnd.osgi.Constants;
+import aQute.bnd.osgi.Domain;
 
+import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.util.FileUtil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -146,6 +162,235 @@ public class BNDSourceUtil {
 		return content;
 	}
 
+	public static void updateSystemPackagesExtraBND(File portalDir)
+		throws IOException {
+
+		File systemPackagesExtraBNDFile = new File(
+			portalDir,
+			"modules/core/portal-bootstrap/system.packages.extra.bnd");
+
+		String oldContent = FileUtil.read(systemPackagesExtraBNDFile);
+
+		String newContent = _getUpdatedSystemPackagesExtraBNDContent(
+			portalDir, oldContent);
+
+		if (!oldContent.equals(newContent)) {
+			FileUtil.write(systemPackagesExtraBNDFile, newContent);
+
+			System.out.println(
+				"Updated 'modules/core/portal-bootstrap" +
+					"/system.packages.extra.bnd'");
+		}
+	}
+
+	private static Map<String, Set<String>> _getPackageVersionsMap(
+			File portalDir, String... dirNames)
+		throws IOException {
+
+		Map<String, Set<String>> packageVersionsMap = new HashMap<>();
+
+		for (String dirName : dirNames) {
+			File dependenciesPropertiesFile = new File(
+				portalDir, dirName + "dependencies.properties");
+
+			if (!dependenciesPropertiesFile.exists()) {
+				continue;
+			}
+
+			Properties properties = new Properties();
+
+			properties.load(new FileInputStream(dependenciesPropertiesFile));
+
+			for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+				File jarFile = new File(
+					portalDir, dirName + (String)entry.getKey() + ".jar");
+
+				if (!jarFile.exists()) {
+					continue;
+				}
+
+				Domain domain = Domain.domain(jarFile);
+
+				if ((domain != null) &&
+					(domain.getBundleSymbolicName() != null)) {
+
+					Parameters exportPackageParameters =
+						domain.getExportPackage();
+
+					for (Map.Entry<String, Attrs> curEntry :
+							exportPackageParameters.entrySet()) {
+
+						Attrs attrs = curEntry.getValue();
+
+						_addPackageVersion(
+							packageVersionsMap, curEntry.getKey(),
+							attrs.getVersion());
+					}
+
+					continue;
+				}
+
+				String version = _getVersion((String)entry.getValue());
+
+				if (version == null) {
+					continue;
+				}
+
+				JarInputStream jarInputStream = null;
+
+				try {
+					jarInputStream = new JarInputStream(
+						new FileInputStream(jarFile));
+
+					while (true) {
+						JarEntry jarEntry = jarInputStream.getNextJarEntry();
+
+						if (jarEntry == null) {
+							break;
+						}
+
+						String name = jarEntry.getName();
+
+						if (name.endsWith(StringPool.SLASH) ||
+							!name.contains(StringPool.SLASH)) {
+
+							continue;
+						}
+
+						String packageName = StringUtil.replace(
+							name.substring(0, name.lastIndexOf(CharPool.SLASH)),
+							CharPool.SLASH, CharPool.PERIOD);
+
+						if (!packageVersionsMap.containsKey(packageName)) {
+							_addPackageVersion(
+								packageVersionsMap, packageName, version);
+						}
+					}
+				}
+				finally {
+					if (jarInputStream != null) {
+						jarInputStream.close();
+					}
+				}
+			}
+		}
+
+		return packageVersionsMap;
+	}
+
+	private static void _addPackageVersion(
+		Map<String, Set<String>> packageVersionsMap, String packageName,
+		String version) {
+
+		Set<String> versions = packageVersionsMap.get(packageName);
+
+		if (versions == null) {
+			versions = new HashSet<>();
+		}
+
+		versions.add(version);
+
+		packageVersionsMap.put(packageName, versions);
+	}
+
+	private static String _getUpdatedSystemPackagesExtraBNDContent(
+			File portalDir, String content)
+		throws IOException {
+
+		Map<String, Set<String>> packageVersionsMap = _getPackageVersionsMap(
+			portalDir, "lib/development/", "lib/global/", "lib/portal/");
+
+		Matcher matcher = _exportVersionPattern.matcher(content);
+
+		while (matcher.find()) {
+			String packageName = matcher.group(1);
+
+			Set<String> versions = packageVersionsMap.get(packageName);
+
+			if (versions == null) {
+				if (!packageName.endsWith(".*")) {
+					continue;
+				}
+
+				versions = new HashSet<>();
+
+				packageName = packageName.substring(
+					0, packageName.length() - 2);
+
+				for (Map.Entry<String, Set<String>> entry :
+						packageVersionsMap.entrySet()) {
+
+					String key = entry.getKey();
+
+					if (key.startsWith(packageName)) {
+						versions.addAll(entry.getValue());
+					}
+				}
+			}
+
+			if (versions.isEmpty()) {
+				continue;
+			}
+
+			String version = matcher.group(2);
+
+			Iterator<String> iterator = versions.iterator();
+
+			if (versions.size() == 1) {
+				String match = matcher.group();
+
+				String replacement = StringUtil.replaceLast(
+					match, version, iterator.next());
+
+				content = StringUtil.replace(content, match, replacement);
+
+				continue;
+			}
+
+			if (versions.contains(version)) {
+				continue;
+			}
+
+			StringBundler sb = new StringBundler();
+
+			sb.append("Incorrect version '");
+			sb.append(version);
+			sb.append("' for '");
+			sb.append(packageName);
+			sb.append("'. Valid candidates are: '");
+
+			while (iterator.hasNext()) {
+				sb.append(iterator.next());
+				sb.append("', '");
+			}
+
+			sb.setIndex(sb.index() - 1);
+
+			sb.append("'. modules/core/portal-bootstrap/");
+			sb.append("system.packages.extra.bnd");
+
+			System.out.println(sb.toString());
+		}
+
+		return content;
+	}
+
+	private static String _getVersion(String s) {
+		int x = s.lastIndexOf(CharPool.COLON);
+
+		if (x == -1) {
+			return null;
+		}
+
+		Matcher matcher = _versionPattern.matcher(s.substring(x + 1));
+
+		if (matcher.find()) {
+			return matcher.group(1);
+		}
+
+		return null;
+	}
+
 	private static Map<String, String> _populateDefinitionKeysMap(
 		String[] keys) {
 
@@ -202,5 +447,10 @@ public class BNDSourceUtil {
 	private static final String[] _SUITE_BND_DEFINITION_KEYS = {
 		"Liferay-Releng-Suite-Description", "Liferay-Releng-Suite-Title"
 	};
+
+	private static final Pattern _exportVersionPattern = Pattern.compile(
+		"\t(.*);version='(.*)',");
+	private static final Pattern _versionPattern = Pattern.compile(
+		"^([0-9]+(\\.[0-9]+){0,2})(\\.[^.]+)?$");
 
 }
