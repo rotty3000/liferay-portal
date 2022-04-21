c79a873b9cb554c5ba37f4474796ef83e4c0cd7e
Gregory Amerson <gregory.amerson@liferay.com>
2022-04-21 00:35:21 -0500

    LCD-13892 show message whether signed in or not

--- workspaces/kubernetes-native-dxp/lxc-extensions/liferay-hello-world/src/routes/hello-world/pages/HelloWorld.js
+++ workspaces/kubernetes-native-dxp/lxc-extensions/liferay-hello-world/src/routes/hello-world/pages/HelloWorld.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HelloWorld = () => (
 	<div className="hello-world">
 		<h1>Hello <span className="hello-world-name">World</span></h1>
-		<p className="random-number"></p>
+		<p>If signed in, and user is authorized, a random number should appear &#10145;&#65039; <span className="random-number"></span></p>
 	</div>
 );
 
