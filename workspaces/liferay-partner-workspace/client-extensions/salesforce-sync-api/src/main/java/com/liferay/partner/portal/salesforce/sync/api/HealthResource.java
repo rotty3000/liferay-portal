c5442a84d584d83e174ca3c52c9d8f50dc80ca52
Raymond Augé <raymond.auge@liferay.com>
2023-02-10 16:19:43 -0500

    don't put this code here. it will break LXC health check

--- workspaces/liferay-partner-workspace/client-extensions/salesforce-sync-api/src/main/java/com/liferay/partner/portal/salesforce/sync/api/HealthResource.java
+++ workspaces/liferay-partner-workspace/client-extensions/salesforce-sync-api/src/main/java/com/liferay/partner/portal/salesforce/sync/api/HealthResource.java
@@ -1,7 +1,5 @@
 package com.liferay.partner.portal.salesforce.sync.api;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthResource {
 
   @GetMapping("/")
-  public String ready(@AuthenticationPrincipal Jwt jwt) throws Exception {
-    System.out.println("============================\n");
-    System.out.println("JWT: " + jwt);
-    System.out.println("\n============================");
-
+  public String ready() throws Exception {
     return "READY";
   }
 }
